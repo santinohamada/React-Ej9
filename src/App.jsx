3602e85319ccef52a14e35facf328ff87ec90203
SantinoHamada <hamadasantino@hotmail.com>
2024-09-03 19:48:34 -0300

    Inicio del repositorio

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+/*
+Crear una aplicación web que permite cargar un formulario para registrar una cita en una veterinaria, todos los datos solicitados en el formulario deben ser obligatorios.
+La aplicación debe permitir borrar las citas y estas deben ser guardadas en el localstorage.
+
+Si hay citas debo mostrarlas, con sus respectivos datos dentro de una card:
+*/
+function App() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
